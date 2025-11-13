2ebc4a6c7c33eed55924f0ecffb7c77c91357ad0
colombefioren <colomberakotonjanahary@gmail.com>
2025-11-13 11:09:02 +0300

    chore: initialize project

---
+++ src/main/java/com/echow/EchowApplication.java
@@ -0,0 +1,12 @@
+package com.echow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchowApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EchowApplication.class, args);
+  }
+}
---
+++ src/test/java/com/echow/EchowApplicationTests.java
@@ -0,0 +1,11 @@
+package com.echow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchowApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
