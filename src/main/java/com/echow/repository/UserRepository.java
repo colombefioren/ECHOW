503f11b7c6bf7383ef03b8bdc059a5eb9ce5ee9b
colombefioren <colomberakotonjanahary@gmail.com>
2025-11-19 11:02:50 +0300

    feat: implement loadUserByUsername method correctly

--- src/main/java/com/echow/repository/UserRepository.java
+++ src/main/java/com/echow/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.echow.repository;
 
 import com.echow.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-  User findByUsername(String username);
+  Optional<User> findByUsername(String username);
 }
