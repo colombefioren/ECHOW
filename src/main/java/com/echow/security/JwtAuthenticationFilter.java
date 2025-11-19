304bb46a72d9c7c87eaff4cef7d62b5bddc89310
colombefioren <colomberakotonjanahary@gmail.com>
2025-11-19 17:43:47 +0300

    refactor: annotate token provider and custom userDetails service with autowired in jwt authenticate fitler

--- src/main/java/com/echow/security/JwtAuthenticationFilter.java
+++ src/main/java/com/echow/security/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,9 +16,9 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-  private JwtTokenProvider tokenProvider;
+  @Autowired private JwtTokenProvider tokenProvider;
 
-  private CustomUserDetailsService customUserDetailsService;
+  @Autowired private CustomUserDetailsService customUserDetailsService;
 
   private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 
