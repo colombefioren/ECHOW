d8f144c5adc0a187e15cfdd1246bfb60c7c120db
colombefioren <colomberakotonjanahary@gmail.com>
2025-11-19 10:52:51 +0300

    refactor: return correct values in user principal method and use lombok

--- src/main/java/com/echow/security/UserPrincipal.java
+++ src/main/java/com/echow/security/UserPrincipal.java
@@ -4,30 +4,22 @@ import com.echow.entity.User;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+@AllArgsConstructor
 public class UserPrincipal implements UserDetails {
 
+  @Getter
   private Long id;
   private String username;
+  @Getter
   private String email;
   private String password;
   private Collection<? extends GrantedAuthority> authorities;
-
-  public UserPrincipal(
-      Long id,
-      String username,
-      String email,
-      String password,
-      Collection<? extends GrantedAuthority> authorities) {
-    this.id = id;
-    this.username = username;
-    this.email = email;
-    this.password = password;
-    this.authorities = authorities;
-  }
 
   public static UserPrincipal create(User user) {
     List<GrantedAuthority> authorities =
@@ -41,36 +33,17 @@ public class UserPrincipal implements UserDetails {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of();
+    return authorities;
   }
 
   @Override
   public String getPassword() {
-    return "";
+    return password;
   }
 
   @Override
   public String getUsername() {
-    return "";
+    return username;
   }
 
-  @Override
-  public boolean isAccountNonExpired() {
-    return UserDetails.super.isAccountNonExpired();
-  }
-
-  @Override
-  public boolean isAccountNonLocked() {
-    return UserDetails.super.isAccountNonLocked();
-  }
-
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return UserDetails.super.isCredentialsNonExpired();
-  }
-
-  @Override
-  public boolean isEnabled() {
-    return UserDetails.super.isEnabled();
-  }
 }
