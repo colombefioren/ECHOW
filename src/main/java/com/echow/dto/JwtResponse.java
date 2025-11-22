23508cd93602f62a17521aa128b1a6ab087e96dc
colombefioren <colomberakotonjanahary@gmail.com>
2025-11-22 10:44:19 +0300

    feat: auth related dtos

---
+++ src/main/java/com/echow/dto/JwtResponse.java
@@ -0,0 +1,19 @@
+package com.echow.dto;
+
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private Set<String> roles;
+
+}
