8fb950a60806de5c1a56ef54d4c29d4203d4c654
COCO <colomberakotonjanahary@gmail.com>
2025-11-22 10:25:46 +0300

    Feat/jwt (#3)
    * feat: user entity
    
    * feat: create skill and offer entities
    
    * feat: add not null validations to some User fields
    
    * feat: add relationships between user and the other entities
    
    * feat: entity relationships to offer and skill
    
    * feat: password encoder
    
    * feat: placeholder user principal and custom user details service
    
    * refactor: return correct values in user principal method and use lombok
    
    * feat: user repository interface
    
    * feat: implement loadUserByUsername method correctly
    
    * feat: create security config with authentication manager
    
    * feat: security filters in security config
    
    * refactor: remove unnecessary assignment of custom user details in security filter chain
    
    * feat: add cors configuratino to allow cross origin requests
    
    * feat: add jjwt dependencies for jwt auth
    
    * feat: jwt token provicer to generate and validate tokens
    
    * feat: implement jwt authentication filter
    
    * feat: add the jwt authentication filter to the security chain so that it runs before other filters
    
    * feat: update application.yaml to have jwt secret and expiration values
    
    * refactor: annotate token provider and custom userDetails service with autowired in jwt authenticate fitler

---
+++ src/main/java/com/echow/security/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.echow.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  @Autowired private JwtTokenProvider tokenProvider;
+
+  @Autowired private CustomUserDetailsService customUserDetailsService;
+
+  private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+    try {
+      String jwt = getJwtFromRequest(request);
+      if (jwt != null && tokenProvider.validateToken(jwt)) {
+        Long userId = tokenProvider.getUserIdFromJwt(jwt);
+
+        UserDetails userDetails = customUserDetailsService.loadUserById(userId);
+
+        UsernamePasswordAuthenticationToken authentication =
+            new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities());
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        logger.debug("Set authentication for user user ID: {}", userId);
+      }
+    } catch (Exception ex) {
+      logger.error("Could not set user authentication in security context", ex);
+    }
+
+    filterChain.doFilter(request, response);
+  }
+
+  private String getJwtFromRequest(HttpServletRequest request) {
+    String bearerToken = request.getHeader("Authorization");
+    if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+      return bearerToken.substring(7);
+    }
+    return null;
+  }
+}
