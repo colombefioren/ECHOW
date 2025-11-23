fa5297536bd9d9fe29af29d7799adac82ca51cb8
colombefioren <colomberakotonjanahary@gmail.com>
2025-11-23 10:33:23 +0300

    fix: add bean annotation to custom cors configuration source in security config

--- src/main/java/com/echow/config/SecurityConfig.java
+++ src/main/java/com/echow/config/SecurityConfig.java
@@ -58,6 +58,7 @@ public class SecurityConfig {
     return http.build();
   }
 
+  @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
 
